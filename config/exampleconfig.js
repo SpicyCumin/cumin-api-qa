c45f17346a90b21801b3feb8a7f3283c3351ad35
positivefx <sfy2368@gmail.com>
2022-04-27 14:47:54 -0400

    adding exampleconfig

--- config/exampleconfig.js
+++ config/exampleconfig.js
@@ -1,3 +1,13 @@
-var PORT = 'YOUR PORT HERE';
+const PORT = 'YOUR PORT HERE';
 
-module.exports = { PORT }
+const HOST1 = 'LINK TO DB HOST';
+
+const USER = 'YOUR DB USER';
+
+const PASSWORD = 'YOUR PASSWORD';
+
+const DBPORT = '5432';
+
+const DB = 'DATABASE NAME';
+
+module.exports = { PORT, HOST1, USER, PASSWORD, DBPORT, DB }
